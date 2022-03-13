786f3a667a1925462126782834d39e9457b77593
uri <stam@gmail.com>
2022-03-13 17:37:15 +0200

    first commit

---
+++ fakeLook-starter/Interfaces/IUser.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace fakeLook_starter.Interfaces
+{
+    public interface IUser<T>
+    {
+        public Task<T> Add(T item);
+    }
+}
---
+++ fakeLook-starter/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using fakeLook_dal.Data;
+using fakeLook_models.Models;
+using fakeLook_starter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace fakeLook_starter.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        readonly private DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+        //public async Task<Post> Add(Post item)
+        //{
+        //    var res = _context.Posts.Add(item);
+        //    await _context.SaveChangesAsync();
+        //    return res.Entity;
+        //}
+
+        public Task<User> Add(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Edit(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<User> GetByPredicate(Func<User, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
